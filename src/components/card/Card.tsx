446b2e6658c65e04ba9b921cb70bc1ef59e4d38b
Filip Michalski 229959 <229959@edu.p.lodz.pl>
2023-05-11 21:19:40 +0200

    add Card component

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+import { PropsWithChildren } from "react";
+
+type CardProps = {
+  className?: string;
+};
+
+export const Card = ({ children, className }: PropsWithChildren<CardProps>) => {
+  return (
+    <div
+      className={clsx(
+        "flex w-full flex-col rounded-2xl bg-bg-light-100 px-5 py-3 shadow-card-light",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
