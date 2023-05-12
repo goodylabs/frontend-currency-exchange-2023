cae619cdc87ea16cd90dccd1a186e0bee793e0f3
SquirrelloDev <olejnike2000@gmail.com>
2023-05-12 09:03:01 +0200

    Added styles for currency view

--- src/components/Currencies/CurrencyView.tsx
+++ src/components/Currencies/CurrencyView.tsx
@@ -1,5 +1,6 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faXmark} from "@fortawesome/free-solid-svg-icons";
+import classes from "../../sass/components/CurrencyView.module.scss";
 interface CurrencyViewProps {
     currency: string,
     code: string,
@@ -8,19 +9,29 @@ interface CurrencyViewProps {
 }
 const CurrencyView = ({currency,code,mid, closeModalFn}:CurrencyViewProps) => {
   return (
-      <div>
-          <button onClick={()=> closeModalFn(false)}><FontAwesomeIcon icon={faXmark} inverse/></button>
-          <div>
-              <h3>Currency name:</h3>
-              <p>{currency}</p>
-              <h3>Currency code:</h3>
-              <p>{code}</p>
-              <h3>Current exchange rate:</h3>
-              <p>{mid}</p>
+      <div className={classes.view}>
+          <div className={classes.view__header}>
+              <h2>Details</h2>
+              <button onClick={()=> closeModalFn(false)} className={classes.view__close}><FontAwesomeIcon icon={faXmark} inverse/></button>
           </div>
-          <hr/>
           <div>
-              <h3>Exchange rate history</h3>
+              <div className={classes.view__info}>
+                  <h3>Currency name:</h3>
+                  <p>{currency}</p>
+              </div>
+              <div className={classes.view__info}>
+                  <h3>Currency code:</h3>
+                  <p>{code}</p>
+              </div>
+              <div className={classes.view__info}>
+                  <h3>Current exchange rate:</h3>
+                  <p>1 {code} = {mid} PLN</p>
+              </div>
+
+          </div>
+          <hr className={classes.view__hr}/>
+          <div>
+              <h3>Exchange rate history (last 14 days)</h3>
               {/*//TODO:wykres*/}
           </div>
       </div>
