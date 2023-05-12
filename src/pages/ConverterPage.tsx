527229744e94e511ef513eb5a35132464d86264f
SquirrelloDev <olejnike2000@gmail.com>
2023-05-12 22:42:14 +0200

    Move changeSelectedCurrencies to CurrencySelectable component

--- src/pages/ConverterPage.tsx
+++ src/pages/ConverterPage.tsx
@@ -5,31 +5,21 @@ import Converter from "../components/Converter/Converter";
 import CurrencyBox from "../components/Converter/CurrencyBox";
 import {RatesObject, tableAResponse} from "../types/types";
 import {useState} from "react";
+import ConverterProvider from "../context/ConverterProvider";
 
 const ConverterPage = () => {
   const loaderData = useLoaderData() as tableAResponse[];
   const [selectedCurrencies, setSelectedCurrencies] = useState<RatesObject[]>([]);
     // TODO: set up selected currencies context
-    const changeSelectedCurrencies = (currencyCode: string, checked?: boolean) => {
-        if(selectedCurrencies.length === 5){
-            return;
-        }
-        if(checked){
-            const desiredCurrency = loaderData[0].rates.find(currency => currency.code === currencyCode);
-            setSelectedCurrencies(prevState => [...prevState, desiredCurrency])
-        }
-        else{
-            const filteredCurrencies = selectedCurrencies.filter(currency => currency.code !== currencyCode);
-            setSelectedCurrencies(filteredCurrencies);
-        }
-
-    }
   return(
-      <Wrapper>
-        <ConvertInput/>
-        <Converter selectedCurrencies={selectedCurrencies}/>
-        <CurrencyBox setSelectedCurrencies={changeSelectedCurrencies}/>
-      </Wrapper>
+      <ConverterProvider>
+          <Wrapper>
+              <ConvertInput/>
+              <Converter selectedCurrencies={selectedCurrencies}/>
+              <CurrencyBox/>
+          </Wrapper>
+      </ConverterProvider>
+
   )
 }
 export default ConverterPage;
