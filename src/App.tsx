c69edf8cfa0d27fb8544cc9c9122f726aab06787
SquirrelloDev <olejnike2000@gmail.com>
2023-05-13 23:32:16 +0200

    Fix endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,17 @@ const todayDay = today.getDate();
 const router = createBrowserRouter([
     {path: "/", element: <RootLayout/>, children:[
             {index: true, element: <CurrenciesPage/>, loader: async () =>{
-                const res = await fetch('http://api.nbp.pl/api/exchangerates/tables/A/');
+                const res = await fetch('https://api.nbp.pl/api/exchangerates/tables/A/');
                 const data = await res.json();
                 return data;
                 }},
             {path: 'gold', element: <GoldPage/>, loader: async () =>{
-                    const res = await fetch(`http://api.nbp.pl/api/cenyzlota/${todayYear}-01-01/${todayYear}-0${todayMonth}-${todayDay}/`);
+                    const res = await fetch(`https://api.nbp.pl/api/cenyzlota/${todayYear}-01-01/${todayYear}-0${todayMonth}-${todayDay}/`);
                     const data = await res.json();
                     return data;
                 }},
             {path: 'converter', element: <ConverterPage/>, loader: async () =>{
-                    const res = await fetch('http://api.nbp.pl/api/exchangerates/tables/A/');
+                    const res = await fetch('https://api.nbp.pl/api/exchangerates/tables/A/');
                     const data = await res.json();
                     return data;
                 }}
