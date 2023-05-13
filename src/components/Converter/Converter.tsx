cd46c0ccbc8f84d32914bc3665097e2e48bb5df9
SquirrelloDev <olejnike2000@gmail.com>
2023-05-13 11:25:58 +0200

    Fix styling for Converter.tsx

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -3,16 +3,17 @@ import CurrencyItem from "../Currencies/CurrencyItem";
 import {RatesObject} from "../../types/types";
 import {useContext, useState} from "react";
 import {converterContext, ConverterContext} from "../../context/ConverterProvider";
+import classes from "../../sass/components/Converter.module.scss";
 
 const Converter = () => {
     const converterCtx = useContext<ConverterContext>(converterContext);
 
   return (
       <Card>
-          <h2>Active currencies</h2>
+          <h2 className={classes.converter__heading}>Active currencies</h2>
         <ul>
             <CurrencyItem currency={"Polski złoty"} code={"PLN"} mid={1.0} converterItem/>
-            {converterCtx.selectedCurrencies.length === 0 && <p>Selected currencies will show up here</p>}
+            {converterCtx.selectedCurrencies.length === 0 && <p className={classes.converter__message}>Selected currencies will show up here</p>}
             {converterCtx.selectedCurrencies.map(currency => <CurrencyItem key={currency.code} currency={currency.currency} code={currency.code} mid={currency.mid} converterItem/>)}
         </ul>
       </Card>
