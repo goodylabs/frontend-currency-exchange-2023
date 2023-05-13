0918e9c3110f46eb828888557b6b88b779fa6ed0
SquirrelloDev <olejnike2000@gmail.com>
2023-05-13 20:51:12 +0200

    Add flag object to store flag names

---
+++ src/utility/globals/flags.ts
@@ -0,0 +1,138 @@
+type FlagsObject = {
+    code: string,
+    filename: string
+}[]
+export const flags:FlagsObject = [
+    {
+        filename: './thai.png' ,
+        code: "THB",
+    },
+    {
+        filename: './united-states.png' ,
+        code: "USD",
+    },
+    {
+        filename: './australia.png' ,
+        code: "AUD",
+    },
+    {
+        filename: './hong-kong.png' ,
+        code: "HKD",
+    },
+    {
+        filename: './canada.png' ,
+        code: "CAD",
+    },
+    {
+        filename: './new-zealand.png' ,
+        code: "NZD",
+    },
+    {
+        filename: './sinagpore.png' ,
+        code: "SGD",
+    },
+    {
+        filename: './eruopean-union.png' ,
+        code: "EUR",
+    },
+    {
+        filename: './hungary.png' ,
+        code: "HUF",
+    },
+    {
+        filename: './switzerland.png' ,
+        code: "CHF",
+    },
+    {
+        filename: './united-kingdom.png' ,
+        code: "GBP",
+    },
+    {
+        filename: './ukraine.png' ,
+        code: "UAH",
+    },
+    {
+        filename: './japan.png' ,
+        code: "JPY",
+    },
+    {
+        filename: './czech-republic.png' ,
+        code: "CZK",
+    },
+    {
+        filename: './denmark.png' ,
+        code: "DKK",
+    },
+    {
+        filename: './iceland.png' ,
+        code: "ISK",
+    },
+    {
+        filename: './norway.png' ,
+        code: "NOK",
+    },
+    {
+        filename: './sweden.png' ,
+        code: "SEK",
+    },
+    {
+        filename: './romania.png' ,
+        code: "RON",
+    },
+    {
+        filename: './bulgaria.png' ,
+        code: "BGN",
+    },
+    {
+        filename: './turkey.png' ,
+        code: "TRY",
+    },
+    {
+        filename: './israel.png' ,
+        code: "ILS",
+    },
+    {
+        filename: './chile.png' ,
+        code: "CLP",
+    },
+    {
+        filename: './philippines.png' ,
+        code: "PHP",
+    },
+    {
+        filename: './mexico.png' ,
+        code: "MXN",
+    },
+    {
+        filename: './south-africa.png' ,
+        code: "ZAR",
+    },
+    {
+        filename: './brazil.png' ,
+        code: "BRL",
+    },
+    {
+        filename: './malaysia.png' ,
+        code: "MYR",
+    },
+    {
+        filename: './indonesia.png' ,
+        code: "IDR",
+    },
+    {
+        filename: './india.png' ,
+        code: "INR",
+    },
+    {
+        filename: './south-korea.png' ,
+        code: "KRW",
+    },
+    {
+        filename: './china.png' ,
+        code: "CNY",
+    },
+    {
+        filename: './world.png' ,
+        code: "XDR",
+    }
+]
