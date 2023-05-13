3e3d67d94535831918e049d4704a96d925ae8d18
jaqubm <jaqobgm@gmail.com>
2023-05-13 17:05:56 +0200

    Created Currency Chart from last 14 days

--- src/pages/currency-chart/index.js
+++ src/pages/currency-chart/index.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import './style.css'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+)
 
 class CurrencyChart extends Component {
 
@@ -34,28 +55,56 @@ class CurrencyChart extends Component {
       this.lastCurrency = this.props.currency;
     }
 
-    if (!isLoaded) {
-      return (
-        <div className='loading'>
-          Data is loading!
-        </div>
-      );
-    }
-    else {
+    if (isLoaded) {
       const curCurrency = Currency.rates;
 
+      let curCurrencyDay = new Array();
+      let curCurrencyValue = new Array();
+
+      for (let i=0; i<curCurrency.length; i++) {
+        curCurrencyDay.push('Day ' + (i+1));
+        curCurrencyValue.push(curCurrency.at(i).mid);
+      }
+
+      const data = {
+        labels: curCurrencyDay,
+        datasets: [{
+          label: this.props.currency,
+          data: curCurrencyValue,
+          backgroundColor: 'transparent',
+          borderColor: '#7E89FDff',
+          pointBorderColor: '#7E89FDff',
+          pointBorderWidth: 4,
+          tension: 0.3,
+        }]
+      };
+
+      const options = {
+        responsive: true,
+        plugins: {
+          legend: false
+        },
+        scales: {
+          x: {
+            /*grid: {
+              display: false,
+            }*/
+          },
+          y: {
+            ticks: {
+              stepSize: 0.01,
+            }
+          }
+        }
+      };
+
       return (
-        <div>
+        <div className='cc-chart-container'>
           <p>
             Currency 14 days history
           </p>
-          <ul>
-            {curCurrency.map(item => (
-              <li key={item.effectiveDate}>
-                {item.effectiveDate}: {item.mid}
-              </li>
-            ))}
-          </ul>
+
+          <Line className='cc-chart' data={ data } options={ options }></Line>
         </div>
       );
     }
