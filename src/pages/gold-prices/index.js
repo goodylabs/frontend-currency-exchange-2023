6d95cd167121d4c87b62dc58323b54ae5e9376a5
jaqubm <jaqobgm@gmail.com>
2023-05-13 18:09:39 +0200

    Added gold price from last 30 days chart

--- src/pages/gold-prices/index.js
+++ src/pages/gold-prices/index.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import './style.css'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+)
 
 class GoldPrices extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      items: [],
+      gold: [],
       isLoaded: false,
     }
   }
@@ -16,28 +37,61 @@ class GoldPrices extends Component {
       .then(res => res.json())
       .then(json => {
         this.setState({
-          items: json,
+          gold: json,
           isLoaded: true,
         })
       });
   }
 
   render() {
-    var { isLoaded, items } = this.state;
+    var { isLoaded, gold } = this.state;
 
     if (isLoaded) {
+      console.log(gold);
+
+      let goldDay = [];
+      let goldPrice = [];
+
+      for (let i=0; i<gold.length; i++) {
+        goldDay.push(`Day ${i+1}`);
+        goldPrice.push(gold.at(i).cena);
+      }
+
+      console.log(goldDay);
+      console.log(goldPrice);
+
+      const data = {
+        labels: goldDay,
+        datasets: [{
+          data: goldPrice,
+          backgroundColor: 'transparent',
+          borderColor: '#7E89FDff',
+          pointBorderColor: '#7E89FDff',
+          pointBorderWidth: 4,
+          tension: 0.3,
+        }]
+      };
+
+      const options = {
+        responsive: true,
+        plugins: {
+          legend: false
+        },
+        scales: {
+          y: {
+            ticks: {
+              stepSize: 0.5,
+            }
+          }
+        }
+      };
+
       return (
-        <div>
+        <div className='gp-chart-container'>
           <p>
             <span>Gold</span> prices
           </p>
-            <ul>
-              {items.map(item => (
-                <li key={item.data}>
-                  {item.data} {item.cena}
-                </li>
-              ))}
-            </ul>
+          <Line className='gp-chart' data={ data } options={ options }></Line>
         </div>
       );
     }
