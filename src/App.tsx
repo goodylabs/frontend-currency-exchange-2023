b45852ef0bc41f4a8831a440ed9b1010ff9aebd2
SquirrelloDev <olejnike2000@gmail.com>
2023-05-14 12:10:34 +0200

    Move fetching loaders to seperate functions

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,27 @@ import CurrenciesPage from "./pages/CurrenciesPage";
 import GoldPage from "./pages/GoldPage";
 import ConverterPage from "./pages/ConverterPage";
 
-const today = new Date();
-const todayYear = today.getFullYear();
-const todayMonth = today.getMonth() + 1;
-const todayDay = today.getDate();
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth() + 1;
+const currentDay = currentDate.getDate();
+
+const fetchTableA = async () => {
+    const res = await fetch('https://api.nbp.pl/api/exchangerates/tables/A/');
+    const data = await res.json();
+    return data;
+}
+const fetchGoldFromCurrentYear = async () => {
+    const res = await fetch(`https://api.nbp.pl/api/cenyzlota/${currentYear}-01-01/${currentYear}-0${currentMonth}-${currentDay}/`);
+    const data = await res.json();
+    return data;
+}
 
 const router = createBrowserRouter([
     {path: "/", element: <RootLayout/>, children:[
-            {index: true, element: <CurrenciesPage/>, loader: async () =>{
-                const res = await fetch('https://api.nbp.pl/api/exchangerates/tables/A/');
-                const data = await res.json();
-                return data;
-                }},
-            {path: 'gold', element: <GoldPage/>, loader: async () =>{
-                    const res = await fetch(`https://api.nbp.pl/api/cenyzlota/${todayYear}-01-01/${todayYear}-0${todayMonth}-${todayDay}/`);
-                    const data = await res.json();
-                    return data;
-                }},
-            {path: 'converter', element: <ConverterPage/>, loader: async () =>{
-                    const res = await fetch('https://api.nbp.pl/api/exchangerates/tables/A/');
-                    const data = await res.json();
-                    return data;
-                }}
+            {index: true, element: <CurrenciesPage/>, loader: fetchTableA},
+            {path: 'gold', element: <GoldPage/>, loader: fetchGoldFromCurrentYear},
+            {path: 'converter', element: <ConverterPage/>, loader: fetchTableA}
     ]}
 ])
 
