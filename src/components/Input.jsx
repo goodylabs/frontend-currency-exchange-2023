5382edca482cb7c841a81e0c1feb631197461acf
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-05-14 18:10:38 +0200

    and controlled inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,11 @@
-const Input = ({ placeholder }) => (
+const Input = ({ placeholder, disabled, value, onChange }) => (
   <input
     type="text"
     className="block w-full rounded-lg border border-zinc-300 bg-zinc-50 p-2.5 text-sm text-zinc-900 focus:border-indigo-500 focus:ring-indigo-500"
     placeholder={placeholder}
+    disabled={disabled}
+    value={value}
+    onChange={onChange}
   />
 );
 
--- src/pages/Converter.jsx
+++ src/pages/Converter.jsx
@@ -1,3 +1,4 @@
+import { ArrowsRightLeftIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
 import CurrencySelect from '../components/CurrencySelect';
 import Input from '../components/Input';
@@ -7,6 +8,8 @@ const Converter = () => {
   const { isLoading, error, data } = useCurrencies();
   const [leftCurrency, setLeftCurrency] = useState('');
   const [rightCurrency, setRightCurrency] = useState('');
+  const [leftValue, setLeftValue] = useState();
+  const [rightValue, setRightValue] = useState();
 
   if (isLoading) return 'Ładowanie...';
 
@@ -16,14 +19,25 @@ const Converter = () => {
     <>
       <h1 className="text-5xl font-semibold tracking-wide text-zinc-900">Konwerter</h1>
       <h2 className="mt-3 text-4xl font-bold tracking-wide text-indigo-500"></h2>
-      <div className="mt-12 flex gap-8 rounded-2xl bg-zinc-100 p-8">
+      <div className="mt-12 flex items-center gap-8 rounded-2xl bg-zinc-100 p-8">
         <div className="flex grow flex-col gap-2">
           <CurrencySelect data={data.rates} value={leftCurrency} onChange={setLeftCurrency} />
-          <Input placeholder="Wpisz kwotę" />
+          <Input
+            placeholder="Wpisz kwotę"
+            disabled={!leftCurrency}
+            value={leftValue}
+            onChange={(e) => setLeftValue(e.target.value)}
+          />
         </div>
+        <ArrowsRightLeftIcon className="h-8 w-8 text-indigo-500" />
         <div className="flex grow flex-col gap-2">
           <CurrencySelect data={data.rates} value={rightCurrency} onChange={setRightCurrency} />
-          <Input placeholder="Wpisz kwotę" />
+          <Input
+            placeholder="Wpisz kwotę"
+            disabled={!rightCurrency}
+            value={rightValue}
+            onChange={(e) => setRightValue(e.target.value)}
+          />
         </div>
       </div>
     </>
