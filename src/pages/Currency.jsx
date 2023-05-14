62419f88fb0dddf2f9d929ab8906211e0cdeab9d
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-05-14 12:18:17 +0200

    style currency page

--- src/pages/Currency.jsx
+++ src/pages/Currency.jsx
@@ -1,13 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
+import dayjs from 'dayjs';
 import { useParams } from 'react-router-dom';
 import api from '../services/api';
+import getHistoricalDates from '../utils/getHistoricalDates';
 
 const Currency = () => {
   const { code } = useParams();
+  const date = dayjs();
+  const [startDate, endDate] = getHistoricalDates(date);
   const { isLoading, error, data } = useQuery({
     queryKey: ['currency'],
     queryFn: async () => {
-      const res = await api.get(`/exchangerates/rates/A/${code}`);
+      const res = await api.get(`/exchangerates/rates/A/${code}/${startDate}/${endDate}`);
       return res.data;
     },
   });
@@ -18,8 +22,13 @@ const Currency = () => {
 
   return (
     <>
-      <h1 className="text-5xl font-semibold tracking-wide text-zinc-900">{data.currency}</h1>
-      <span className="mt-12 text-xl font-semibold text-zinc-900">XD</span>
+      <span className="text-3xl font-semibold tracking-wide text-indigo-500">{data.code}</span>
+      <h1 className="mt-3 text-5xl font-semibold tracking-wide text-zinc-900 first-letter:uppercase">
+        {data.currency}
+      </h1>
+      <span className="mt-12 text-xl font-semibold text-zinc-900">
+        Kurs waluty w ciągu ostatnich dwóch tygodni
+      </span>
     </>
   );
 };
