a1147d80b45d062f5f994fb0e3aef9de854bec6a
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-05-14 13:04:11 +0200

    change navbar title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,21 @@ const items = [
 
 const Navbar = () => {
   return (
-    <div className="flex w-full max-w-xs flex-col gap-12 border-r-2 bg-zinc-100 p-8">
-      <span className="px-2.5 text-lg font-semibold">Currency Exchange</span>
+    <div className="flex w-full max-w-xs flex-col gap-12 border-r-2 bg-zinc-100 px-8 py-12">
+      <div className="flex flex-col gap-1">
+        <span className="px-2.5 text-xl font-semibold text-indigo-500">Currency Exchange</span>
+        <span className="text-md px-2.5 font-medium text-zinc-500">
+          by{' '}
+          <a
+            className="hover:underline"
+            href="https://github.com/kwawrzynczak"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @kwawrzynczak
+          </a>
+        </span>
+      </div>
       <nav className="flex flex-col gap-6">
         {items.map((item) => (
           <NavbarItem key={item.name} name={item.name} link={item.link} Icon={item.icon} />
