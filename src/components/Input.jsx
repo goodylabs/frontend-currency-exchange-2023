bfacad703b1b408c8ca5cedcc6b00382a7dbcc40
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-05-14 19:08:44 +0200

    style Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 const Input = ({ placeholder, disabled, value, onChange }) => (
   <input
     type="text"
-    className="block w-full rounded-lg border border-zinc-300 bg-zinc-50 p-2.5 text-sm text-zinc-900 focus:border-indigo-500 focus:ring-indigo-500"
+    className="block h-12 w-full rounded-xl border-2 bg-white px-4 text-zinc-900 outline-none focus:border-indigo-500"
     placeholder={placeholder}
     disabled={disabled}
     value={value}
